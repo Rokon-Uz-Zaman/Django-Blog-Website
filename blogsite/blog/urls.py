50ad4ce75f8e23c2f6364fdf80f21184ef188b70
A.B.M. Rokon-Uz-Zaman Roman <mdroman266@gmail.com>
2022-10-25 13:54:23 +0600

    Update urls.py

--- blogsite/blog/urls.py
+++ blogsite/blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     
     path('',views.bloghome , name = 'bloghome'),
-    path('<str:slug>',views.blogpost , name = 'blogpost')
+    path('<str:slug>',views.blogpost , name = 'blogpost'),
+    path('api/', views.PostList.as_view() , name='postList')
 ]
 
